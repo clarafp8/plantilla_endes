64db31d7ce35837ce318c4852a3d223405e88173
Clara <claraafrigoletp@gmail.com>
2025-03-24 21:04:03 +0100

    Añadido el javaDocs

--- plantilla/src/test/java/com/endes/entidad/ComercialTest.java
+++ plantilla/src/test/java/com/endes/entidad/ComercialTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.miempresa.entidad.Producto;
 
 class ComercialTest {
 	
