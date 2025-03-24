358310ad232f02e8a26432b73e2015b656133bc0
clarafp8 <158521872+clarafp8@users.noreply.github.com>
2025-03-24 21:11:51 +0100

    Borro el doc que no iba en esta rama

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.endes","l":"App"},{"p":"com.endes.entidad","l":"Comercial"},{"p":"com.endes.entidad","l":"Empleado"},{"p":"com.endes.entidad","l":"Plantilla"},{"p":"com.endes.entidad","l":"Tecnico"}];updateSearchResults();
